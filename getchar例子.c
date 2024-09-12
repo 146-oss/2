7d8f6c2de683de82b5b535dd562fc12182bd789a
146-oss <167414612@qq.com>
2024-09-12 23:39:47 +0800

    四.1

---
+++ getchar例子.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+	char ch=getchar();
+//	printf("%c\n",ch);
+    putchar(ch);
+	return 0;
+} 
---
+++ if输出例子2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	int age=0;
+	scanf("%d",&age);
+	if(age<18)
+     printf("少年\n");
+     else if(age>=18&&age<26)
+     printf("青年\n");
+     else if(age>=26&&age<40)
+     printf("中年\n");
+     else if(age>=40&&age<60)
+     printf("壮年\n");
+     else
+     printf("老不死\n"
+	 );
+		
+	
+	return 0;
+} 
