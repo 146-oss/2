699f5fe1f155c7b4d19ba3b3572efb33339e4ca2
146-oss <167414612@qq.com>
2024-10-13 23:56:06 +0800

    八.1

---
+++ 函数的链式访问.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+int main(){
+	int a=strlen("sadas");
+	printf("%d\n",a);
+	printf("%d\n",strlen("sadas"));
+	char arr[20]={0};
+	char arr1[]="llll";
+	strcpy(arr,arr1);
+	printf("%s\n",arr);
+	printf("%s\n",strcpy(arr,arr1));
+	printf("%d",printf("%d",printf("%d",666)));
+	return 0;
+}
